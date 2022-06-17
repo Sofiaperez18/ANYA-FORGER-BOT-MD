42ff2939b178823e5de2754e555e730a8dc7d541
Sofiaperez18 <107665792+Sofiaperez18@users.noreply.github.com>
2022-06-16 23:01:47 -0600

    Add files via upload

---
+++ random-blackpink.js
@@ -0,0 +1,11 @@
+import fetch from 'node-fetch'
+let handler = async(m, { conn, args, usedPrefix, command }) => {
+fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/kpop/blackpink.txt').then(res => res.text()).then(body => {
+let randomkpop = body.split('\n')
+let randomkpopx = randomkpop[Math.floor(Math.random() * randomkpop.length)]
+conn.sendButton(m.chat, `_${command}_`, author, randomkpopx, [['✅ 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 ✅', `/${command}`]], m)
+})}
+handler.help = ['blackpink']
+handler.tags = ['internet']
+handler.command = /^(blackpink)$/i
+export default handler
